4d03e516a878028b78ed635f7361fb8e98136b67
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-10-16 12:39:56 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  resources :tasks
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
