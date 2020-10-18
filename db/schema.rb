41d3ec270ea40d4e7ff14d37c5a506b325927a2d
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-10-18 14:43:52 +0900

    エラー文の表示

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_10_16_032952) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
