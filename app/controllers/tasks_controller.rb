b1b329c482c06e62fffed36241ad91fcd1761344
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-10-19 15:00:42 +0900

    ログインしているユーザーのtaskデータの登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  # before_action :set_task, only: [:edit, :show, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -21,17 +22,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_path
   end
 
   def destroy
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_path
   end
@@ -41,5 +42,9 @@ private
   def task_params
     params.require(:task).permit(:name, :description).merge(user_id: current_user.id)
   end
+
+  # def set_task
+  #   @task = Task.find(params[:id])
+  # end
 
 end
